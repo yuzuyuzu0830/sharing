f947748b7f8be7d1a7916d25c39005d906287ab7
yuzuyuzu0830 <kiki.balon@gmail.com>
2021-02-23 15:12:29 +0900

    SPA開発環境とVue Router

---
+++ project/resources/js/router.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import PhotoList from './pages/PhotoList.vue'
+import Login from './pages/Login.vue'
+
+// VueRouterプラグインを使用する
+// これによって<RouterView />コンポーネントなどを使うことができる
+Vue.use(VueRouter)
+
+// パスとコンポーネントのマッピング
+const routes = [
+  {
+    path: '/',
+    component: PhotoList
+  },
+  {
+    path: '/login',
+    component: Login
+  }
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    routes
+})
+
+// VueRouterインスタンスをエクスポートする
+// app.jsでインポートするため
+export default router
