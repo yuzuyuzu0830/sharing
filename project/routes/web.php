2680f652fd44c61b75e8021e4e79e1e913bcbf3c
yuzuyuzu0830 <kiki.balon@gmail.com>
2021-02-24 23:46:18 +0900

    会員登録APIの実装

--- project/routes/web.php
+++ project/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,3 +13,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/{any?}', fn() => view('index'))->where('any', '.+');
+
+Route::post('/register', 'Auth\RegisterController@register')->name('register');
---
+++ project/tests/Feature/RegisterApiTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\User;
+
+class RegisterApiTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function should_新しいユーザーを作成して返却する()
+    {
+        $data = [
+            'name' => 'vuesplash user',
+            'email' => 'dummy@email.com',
+            'password' => 'test1234',
+            'password_confirmation' => 'test1234',
+        ];
+
+        $response = $this->json('POST', route('register'), $data);
+
+        $user = User::first();
+        $this->assertEquals($data['name'], $user->name);
+
+        $response
+            ->assertStatus(201)
+            ->assertJson(['name' => $user->name]);
+    }
+}
